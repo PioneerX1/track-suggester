6507bdb1d12959673e02f206c3e0a8d37e3a882c
Mick Sexton <lacrookedbeat@hotmail.com>
2017-06-02 10:26:20 -0700

    test initial round of conditional statements with an alert to make sure results are tallying up properly

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,50 @@ $(document).ready(function() {
     var ruby = 0;
     var css = 0;
     var finalResult;
-    var answer;
 
     var answer1 = $("select#q1").val();
+    var answer2 = $("select#q2").val();
+    var answer3 = $("select#q3").val();
+    var answer4 = $("select#q4").val();
+    var answer5 = $("select#q5").val();
 
     if (answer1 === "1a") {
-      alert("1st option selected");
+      css += 1;
     } else if (answer1 === "1b") {
-      alert("2nd option selected");
+      ruby += 1;
     } else if (answer1 === "1c") {
-      alert("3rd option selected");
+      java += 1;
     }
+
+    if (answer2 === "2a") {
+      css += 1;
+    } else if (answer2 === "2b") {
+      ruby += 1;
+      java += 1;
+    }
+
+    if (answer3 === "3a") {
+      ruby += 1;
+    } else if (answer3 === "3b") {
+      java += 1;
+    }
+
+    if (answer4 === "4a") {
+      css += 1;
+    } else if (answer4 === "4b") {
+      ruby += 1;
+      java += 1;
+    }
+
+    if (answer5 === "5a") {
+      java += 1;
+    } else if (answer4 === "5b") {
+      ruby += 1;
+    } else if (answer4 === "5c") {
+      css += 1;
+    }
+
+    alert("css is " + css + " ruby is " + ruby + " and java is " + java);
 
 
     //CODE FOR DETERMINING RESULTS
